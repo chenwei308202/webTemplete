375c65a474731046c97f126ee010b7ab7ce0df17
chenwei <609605093@qq.com>
2017-11-02 14:41:04 +0800

    junit配置

---
+++ test-web/src/test/java/test/service/ServiceTest.java
@@ -0,0 +1,17 @@
+package test.service;
+
+import org.junit.Test;
+
+import test.base.BaseJunitTest;
+
+
+
+public class ServiceTest extends BaseJunitTest{
+	
+	@Test
+	public void testMethod(){
+		
+	}
+	
+	
+}
